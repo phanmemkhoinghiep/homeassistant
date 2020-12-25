bd5ffd7d773e04f2c60a68cbc960c5b07bf1cdad
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2020-12-25 17:21:29 +0700

    Update __init__.py

--- custom_components/tts_zalo/__init__.py
+++ custom_components/tts_zalo/__init__.py
@@ -24,25 +24,25 @@ def setup(hass, config):
 
     def tts_handler(data_call):
         # Get config
-        zalo_api = str(config[DOMAIN][CONF_API_KEY])                
-        url_hass = str(config[DOMAIN][CONF_URL_HASS])        
+        apikey = str(config[DOMAIN][CONF_API_KEY])                
         # Get data service
         media_id = data_call.data.get(CONF_PLAYER_ID)
-        text_message = str(data_call.data.get(CONF_MESSAGE)[0:2000])
+        message = str(data_call.data.get(CONF_MESSAGE)[0:2000])
         voice_id = data_call.data.get(CONF_VOICE_ID)
         speed = data_call.data.get(CONF_SPEED)
         # List voice of Zalo Speech Synthesis
         voice_id_list = {'nu_mien_bac_01': 2, 'nam_mien_bac_01': 4, 'nu_mien_nam_01': 1,'nam_mien_nam_02': 3}
-        sepaker_id= voice_id_list.get(voice_id)
+        speaker_id= voice_id_list.get(voice_id)
         encode_type= 1
         # HTTP Request
         url = 'https://api.zalo.ai/v1/tts/synthesize'
         # Header Parameters
-        header_parameters = {'apikey': zalo_api}
+        header_parameters = {'apikey': apikey}
         # Body Parameters
-        data = {'input': text_message.encode('utf-8'), 'speed': speed_read, 'encoder_type': encode_type,'speaker_id': speaker_id}
+        data = {'input': message.encode('utf-8'), 'speed': speed, 'encoder_type': encode_type,'speaker_id': speaker_id}
         # Get response from Server
-        url_file = requests.post(url, data = text_message, headers = header_parameters).json()['async']
+        response = requests.post(url, data = data, headers = header_parameters)
+        url_file = response.json()['data']['url']
         # "The content will be returned after a few seconds under the async link, wait some seconds"
         #time.sleep(2)
         # service data for 'CALL SERVICE' in Home Assistant
