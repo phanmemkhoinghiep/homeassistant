c28e203b6b3394565abfefea416cbd7a883a705f
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2020-12-25 13:18:29 +0700

    Update __init__.py

--- custom_components/tts_zalo/__init__.py
+++ custom_components/tts_zalo/__init__.py
@@ -1,1 +1,47 @@
-sfdsf
+# encoding: utf-8
+
+
+# Declare variables
+DOMAIN = 'tts_zalo'
+SERVICE_ZALO_TTS = 'say'
+# config
+CONF_API_KEY = 'api_key'
+CONF_SPEED = 'speed'
+# data service
+CONF_PLAYER_ID = 'entity_id'
+CONF_MESSAGE = 'message'
+CONF_VOICE_ID = 'voice_id'
+
+import requests, json, os, time, uuid, urllib, datetime
+
+def setup(hass, config):
+
+    def tts_handler(data_call):
+        # Get config
+        fpt_api = str(config[DOMAIN][CONF_API_KEY])                
+        # Get data service
+        media_id = data_call.data.get(CONF_PLAYER_ID)
+        text_message = str(data_call.data.get(CONF_MESSAGE)[0:2000])
+        voice_id = data_call.data.get(CONF_VOICE_ID)
+        speed = data_call.data.get(CONF_SPEED)
+        # List voice of FPT Speech Synthesis
+        voice_id_list = {'nu_mien_bac_01': 2, 'nam_mien_bac_01': 4, 'nu_mien_nam_01': 1,'nam_mien_nam_02': 3}
+        sepaker_id= voice_id_list.get(voice_id)
+        encoder_type= 1
+        # HTTP Request
+        url = 'https://api.zalo.ai/v1/tts/synthesize'
+        # Header Parameters
+        header_parameters = {'api_key': zalo_api}
+        # Body Parameters
+        text_message = {'input': text_message.encode('utf-8'), 'speed': speed_read, 'speaker_id': 1, 'voice': speaker_id}
+        # Get response from Server
+        url_file = requests.post(url, data = text_message, headers = header_parameters).json()['async']
+        # "The content will be returned after a few seconds under the async link, wait some seconds"
+        time.sleep(2)
+        # service data for 'CALL SERVICE' in Home Assistant
+        service_data = {'entity_id': media_id, 'media_content_id': url_file, 'media_content_type': 'audio/mp3'}
+        # Call service from Home Assistant
+        hass.services.call('media_extractor', 'play_media', service_data)
+        
+    hass.services.register(DOMAIN, SERVICE_FPT_TTS, tts_handler)
+    return True
